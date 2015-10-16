b8c637f94fbdf58d68389ed717c2a5b1705140b4
Saju Thankappan <sajuthankappan@gmail.com>
2015-10-16 22:25:22 +0530

    fixed tests code to reflect crate name change

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,33 +1,33 @@
-extern crate urlshortener_converter;
+extern crate urlshortener_codec;
 
-use urlshortener_converter as converter;
+use urlshortener_codec as codec;
 
 #[test]
 fn encode_for_zero_returns_a() {
-    let alias = converter::encode(0);
+    let alias = codec::encode(0);
     assert_eq!("a", alias);
 }
 
 #[test]
 fn encode_for_1_returns_b() {
-    let alias = converter::encode(1);
+    let alias = codec::encode(1);
     assert_eq!("b", alias);
 }
 
 #[test]
 fn encode_for_1000_returns_qi() {
-    let alias = converter::encode(1000);
+    let alias = codec::encode(1000);
     assert_eq!("qi", alias);
 }
 
 #[test]
 fn decode_for_qi_returns_1000() {
-    let i = converter::decode("qi".to_string());
+    let i = codec::decode("qi".to_string());
     assert_eq!(1000, i);
 }
 
 #[test]
 fn decode_for_a_returns_0() {
-    let i = converter::decode("a".to_string());
+    let i = codec::decode("a".to_string());
     assert_eq!(0, i);
 }
